2039cfb435a46de8a63077267b7f3933febde0e3
HMalik <hmalik-@hotmail.com>
2023-02-16 21:05:28 +0000

    added a for each loop to search and adding objects to tree

--- src/main/java/org/example/SerializationProject/model/NodeInputter.java
+++ src/main/java/org/example/SerializationProject/model/NodeInputter.java
@@ -3,7 +3,11 @@ import java.util.List;
 
 // Takes the list of Employee objects and adds them to the binary tree.
 public class NodeInputter {
-    public static void inputNodes(List<Employee> list) {
-
+    public static void inputNodes(List<Employee> list, BinaryTree tree)  {
+        //Created an instance of BinaryTree in the parameter and there is also a list of all employees which is listed in the parameters
+        for (Employee employee : list) {
+            tree.addElement(employee);
+            // for each employee object in the list we call the "addElement" method of the BinaryTree instance to add the employee to the tree
+        }
     }
 }
