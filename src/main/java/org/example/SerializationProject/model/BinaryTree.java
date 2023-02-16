791edae4a1e9d06062de56e9256a0b1f6455e80c
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-02-16 17:52:56 +0000

    Completed Binary Tree, Recrsive and ChildNotFoundException classes

--- src/main/java/org/example/SerializationProject/model/BinaryTree.java
+++ src/main/java/org/example/SerializationProject/model/BinaryTree.java
@@ -8,12 +8,44 @@ public class BinaryTree implements IBinaryTree{
 
     @Override
     public void addElement(Employee element) {
-
+        if (root == null) {
+            root = new Node(element);
+            tempNode = root;
+        }
+        if (element.getEmployeeID() < tempNode.getValue().getEmployeeID()) {
+            if (tempNode.isLeftChildEmpty()) {
+                tempNode.setLeftChild(new Node(element));
+            } else {
+                tempNode = tempNode.getLeftChild();
+                addElement(element);
+            }
+        } else if (element.getEmployeeID() > tempNode.getValue().getEmployeeID()) {
+            if (tempNode.isRightChildEmpty()) {
+                tempNode.setRightChild(new Node(element));
+            } else {
+                tempNode = tempNode.getRightChild();
+                addElement(element);
+            }
+        }
+        tempNode = root;
     }
 
     @Override
     public boolean findEmployeeByID(int id) {
+        if (root == null ) {
+            return false;
+        } else if (root.getValue().getEmployeeID() == id) {
+            return true;
+        }
+        // If returns true ... Then call similar method to return an Employee
+        tempNode = root;
+        boolean answer = Recursive.recursiveCall(id);
+        tempNode = root;
 
-        return false;
+        // Similar method call goes here.
+        // Should return an employee.
+
+        return answer;
+
     }
 }
--- src/main/java/org/example/SerializationProject/model/Recursive.java
+++ src/main/java/org/example/SerializationProject/model/Recursive.java
@@ -1,4 +1,6 @@
 package org.example.SerializationProject.model;
+import static org.example.SerializationProject.model.BinaryTree.root;
+import static org.example.SerializationProject.model.BinaryTree.tempNode;
 
 // Traverses the binary tree
 public class Recursive {
@@ -6,6 +8,20 @@ public class Recursive {
         // Check if root = null   -> throw ChildNotFoundException
         // else continue
 
+        if (value < tempNode.getValue().getEmployeeID()) {
+            // check if tempNode has left child
+            if (tempNode.getLeftChild().getValue().getEmployeeID() == value) {
+                return true;
+            }
+            tempNode = tempNode.getLeftChild();
+            return recursiveCall(value);
+        } else if (value > tempNode.getValue().getEmployeeID()) {
+            if (tempNode.getRightChild().getValue().getEmployeeID() == value) {
+                return true;
+            }
+            tempNode = tempNode.getRightChild();
+            return recursiveCall(value);
+        }
         return false;
     }
 }
